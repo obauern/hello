cc9873b1fd626f0d87cbc17b67cb75f5d4dbed6e
obauern <obauern@gmail.com>
2021-05-08 18:07:32 +0000

    que te importa

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Autor: Alejandro Bauernfeind
+ * 07/05/2021
+ * */
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(){
+     
+     printf("Hello world!\n");
+     
+     return 0;
+ }
